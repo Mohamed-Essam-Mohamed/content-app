b3bfd050f933ddbe3f45a9956bc9a81c869c33b0
Mohamed-Essam-Mohamed <muhammed.essam.eid@gmail.com>
2026-05-04 20:33:18 +0300

    project created

---
+++ app/src/main/java/com/example/app_contact/HomeActivity.kt
@@ -0,0 +1,17 @@
+package com.example.app_contact
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.app_contact.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
